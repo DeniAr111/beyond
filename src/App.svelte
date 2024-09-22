df91ccb2e990343bc198b8e8680cae887c66f3a5
Visakh <visakhkuroor@gmail.com>
2024-09-22 07:48:15 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,82 @@
+<script>
+	
+</script>
+
+
+	<body>
+		<main>
+    <br>
+        <h1 class="heading">
+            Fuel Your Goals with <span class="headingpt2">Beyond Innovation</span>
+        </h1>
+    <div> 
+    <form>   
+	<label for="name">Name</label><br>
+    <input style="color: white;" type="text" id="name" class="bg" size="75px" placeholder="____________________________________________________"><br><br>
+	<label for="phno">Phone number</label><br>
+    <input  style="color: white;" type="text" id="phno" class="bg" size="75px" placeholder="____________________________________________________"><br><br>
+	<label for="email">Email</label><br>
+    <input  style="color: white;" type="text" id="email" class="bg" size="75px" placeholder="____________________________________________________"><br><br>
+    <label for="msg">Message</label><br>
+    <input  style="color: white;" type="text" id="msg" class="bg" size="75px" placeholder="____________________________________________________"><br><br>
+    <button class="nn">Send Enquiry</button>
+    </form>
+    </div>
+</main>
+</body>
+
+
+<style>
+body{
+	background: linear-gradient(114.9deg, rgb(34, 34, 34) 8.3%, rgb(0, 40, 60) 41.6%, rgb(0, 143, 213) 93.4%);
+	height: 100%;
+	font-family: "Roboto", sans-serif;
+}
+.heading{
+     text-align: center;
+     font-size: 40;
+     color: white;
+     font-family:  Arial, sans-serif;
+}
+.headingpt2{
+     color: rgb(88, 88, 205);
+     text-align: center;
+}
+div{
+	display: flex;
+	justify-content: center;
+     color: white;
+     text-align: left;
+     font-size: 15px;
+     margin-left: 10px;
+     margin-top: 100px;
+
+}
+.bg{
+     background: transparent;
+     border: none;
+}
+.nn{
+     background: transparent;
+     border: none;
+     color: white;
+     font-size: 24px;
+     text-align: center;
+     margin-left: 170px;
+}
+#name {
+     width: 500px;
+    
+}
+#phno {
+     width: 500px;
+     
+}
+#email {
+     width: 500px;
+     
+}
+#msg{
+     width: 500px;
+}
+</style>
